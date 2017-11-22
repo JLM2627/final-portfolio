69a7efffc64c78953e2f5c202ffdae2a1a22f8af
Jayme Marshall <arcal619@gmail.com>
2017-11-21 20:56:44 -0500

    Rendering Homepage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+
+const Title = styled.h1`
+padding: 100px 0 0 0;
+text-align: center;
+font-family: 'Monoton';
+font-size: 38px;
+color: white;
+opacity: 0.6;
+`
+
 
 class Home extends Component {
     render() {
         return (
             <div>
-                
+                <Title><h1>This is the homepage</h1></Title>
+               
             </div>
         );
     }
