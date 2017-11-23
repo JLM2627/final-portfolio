501125deee871b12d45478f257eef61ca1e79fb3
Jayme Marshall <arcal619@gmail.com>
2017-11-23 16:29:21 -0500

    refactoring navbar

--- src/components/static/NavBar.js
+++ src/components/static/NavBar.js
@@ -2,42 +2,35 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
 
-const StyledNavBar = styled.div`
- display: -webkit-flex;
-   display: flex;
-   -webkit-flex-direction: row;
-   flex-direction: row;
-   -webkit-justify-content: flex-end;
-   justify-content: flex-end;
+const NavBarContainer = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: flex-end;
    margin: 10px;
    font-family: 'Josefin Slab';
- /* display: flex;
- justify-content: space-between;
-align-items: left; */
 
-/* text-decoration: none;
-text-align:right;
-color: white; */
 `
-const LinkStyle = styled.div`
+const LinkDescription = styled.div`
  display: inline;
-/* float: right; */
-
 opacity: 0.4;
 margin: 10px;
+a {
+    text-decoration: none;
+    color: white;
+}
 `;
 
 class NavBar extends Component {
     render() {
         return (
-            <StyledNavBar>
+            <NavBarContainer>
             <div>
-                <LinkStyle><Link to="/"style={{ textDecoration: 'none', color: 'white' }}>Home</Link></LinkStyle>
-                <LinkStyle><Link to="/projects"style={{ textDecoration: 'none', color: 'white' }}>Projects</Link></LinkStyle>
-                    <LinkStyle><Link to="/about" style={{ textDecoration: 'none', color: 'white' }}>About</Link></LinkStyle>
-                    <LinkStyle><Link to="/contact"style={{ textDecoration: 'none', color: 'white' }}>Contact</Link></LinkStyle>    
+                <LinkDescription><Link to="/"><a>Home</a></Link></LinkDescription>
+                <LinkDescription><Link to="/projects"><a>Projects</a></Link></LinkDescription>
+                    <LinkDescription><Link to="/about"><a>About</a></Link></LinkDescription>
+                    <LinkDescription><Link to="/contact"><a>Contact</a></Link></LinkDescription>    
             </div>
-            </StyledNavBar>
+            </NavBarContainer>
         );
     }
 }
